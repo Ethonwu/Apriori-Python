4571e75f3a12721adaefce8a69afdbff87121ee8
a5351998 <a5351998@gmail.com>
2017-08-08 21:39:21 +0800

    Add Prune imemset part

--- Apriori/Apriori.py
+++ Apriori/Apriori.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import copy
+minsupport = 4
 C1={} #The dict {} in python such as:my_information = {'name': 'Pusheen the Cat', 'country': 'USA', 'favorite_numbers': [42, 105]}
       #name -> Pusheen the Cat (Key is name , value is Pusheen the cat)
 file = open('example.txt')
@@ -10,7 +12,17 @@ for line in file:
             C1[item] = 1
 print C1.keys()
 print C1.values()
+#one_itemsets=copy.copy(C1.values())
+#print one_itemsets
+L1 = []
+lenght = len(C1.keys())
+for i in C1:
+    if C1[i] >= minsupport:
+        L1.append(i)
+print L1
 
+
+    
             
 
 
