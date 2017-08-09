6668020a2c657f57e46b33a0f8dcb82efaf35df4
a5351998 <a5351998@gmail.com>
2017-08-10 00:03:50 +0800

    Add Apriori_gen part

--- Apriori/Apriori.py
+++ Apriori/Apriori.py
@@ -4,13 +4,12 @@ minsupport = 4
 C1={} 
 #The dict {} in python such as:my_information = {'name': 'Pusheen the Cat', 'country': 'USA', 'favorite_numbers': [42, 105]}
 #name -> Pusheen the Cat (Key is name , value is Pusheen the cat)
-def Apriori_gen(Itemset, k):
-    candidate = []
-    for i in Itemset:
-        for s in i:
-            strr = str(s)
-            print strr.index(1)
+def Apriori_gen(Itemset, k, lenght):
+    canditate = []
+    for i in range (0,lenght):
 
+            element = str(Itemset[i])
+            print element[0:(len(element)-1)]
 
 file = open('example.txt')
 for line in file:
@@ -26,16 +25,14 @@ print C1.values()
 #one_itemsets=copy.copy(C1.values())
 #print one_itemsets
 L1 = []
+#L1 = set()
 for i in C1:
     if C1[i] >= minsupport:
         L1.append(i)
 L1.sort()
 print L1
 k=2
-L2 = [ 'abc', 'bc', 'abd']
-Apriori_gen(L2, k)
-    
-            
-
-
-
+l = len(L1)
+L2 = [ 'abc' , 'abe' ]
+#L2 = Apriori_gen(L1, k ,l)
+Apriori_gen(L2, k ,2)
