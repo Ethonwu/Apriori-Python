e7060f3a4e47a7e09cc8ed82fa23283bb91bb0d9
a5351998 <a5351998@gmail.com>
2017-08-09 21:05:00 +0800

    Debug

--- Apriori/Apriori.py
+++ Apriori/Apriori.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 import copy
 minsupport = 4
-C1={} #The dict {} in python such as:my_information = {'name': 'Pusheen the Cat', 'country': 'USA', 'favorite_numbers': [42, 105]}
-      #name -> Pusheen the Cat (Key is name , value is Pusheen the cat)
+C1={} 
+#The dict {} in python such as:my_information = {'name': 'Pusheen the Cat', 'country': 'USA', 'favorite_numbers': [42, 105]}
+#name -> Pusheen the Cat (Key is name , value is Pusheen the cat)
+def Apriori_gen(Itemset, k):
+    candidate = []
+    for i in Itemset:
+        for s in i:
+            strr = str(s)
+            print strr.index(1)
+
+
 file = open('example.txt')
 for line in file:
     for item in line.split():
@@ -10,18 +19,21 @@ for line in file:
             C1[item] +=1
         else:
             C1[item] = 1
+C1.keys().sort()
+C1.values().sort()
 print C1.keys()
 print C1.values()
 #one_itemsets=copy.copy(C1.values())
 #print one_itemsets
 L1 = []
-lenght = len(C1.keys())
 for i in C1:
     if C1[i] >= minsupport:
         L1.append(i)
+L1.sort()
 print L1
-
-
+k=2
+L2 = [ 'abc', 'bc', 'abd']
+Apriori_gen(L2, k)
     
             
 
